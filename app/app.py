1b83ed50783c9ed7835bff009a58c8daaae5331c
Earon Pabon <pabs@migo.tv>
2020-02-02 10:33:52 +0800

    external visible server

--- app/app.py
+++ app/app.py
@@ -202,5 +202,5 @@ def add_contribution():
     # save contribution data to database
 
 if __name__ == "__main__":
-    app.run(ssl_context="adhoc")
+    app.run(host=0.0.0.0, ssl_context="adhoc")
     
