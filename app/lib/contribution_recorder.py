f39a91fb632044c16c8d7bf10c8cb7cec8692bed
Name here <earon.pabon@gmail.com>
2020-02-13 13:41:01 +0800

    Upload API with google

--- app/lib/contribution_recorder.py
+++ app/lib/contribution_recorder.py
@@ -1,3 +1,5 @@
+from apiclient.http import MediaFileUpload
+
 
 class ContributionRecorder:
     def __init__(self, drive):
@@ -10,3 +12,10 @@ class ContributionRecorder:
         contributions = results.get('files', [])
 
         return contributions
+
+    def addContribution(self, filepath, mimetype, file_metadata):
+        media = MediaFileUpload(filepath, mimetype=mimetype)
+        file = self.drive.files().create(
+            body=file_metadata, media_body=media, fields='id').execute()
+
+        return file.get('id')
